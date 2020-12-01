bc6a36d1ed5c7fdc2b91ce135277b0b32a8260c8
Henrikas <henrikasju@gmail.com>
2020-12-01 17:41:21 +0200

    Adding detail collectino cell view

---
+++ CryptoChecker/Views/CollectionView/CollectionViewCells/CurrencyDetailCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CurrencyDetailCollectionViewCell.swift
+//  CryptoChecker
+//
+//  Created by Henrikas J on 01/12/2020.
+//
+
+import UIKit
+
+class CurrencyDetailCollectionViewCell: UICollectionViewCell {
+    
+}
