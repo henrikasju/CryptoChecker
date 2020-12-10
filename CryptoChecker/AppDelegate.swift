a08a74feb699796aeb611ee8ef8d43ca67012d4a
Henrikas <henrikasju@gmail.com>
2020-12-10 16:27:27 +0200

    Adding UserDefault Manager

--- CryptoChecker/AppDelegate.swift
+++ CryptoChecker/AppDelegate.swift
@@ -16,36 +16,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let defaults = UserDefaults.standard
         
         // if App launched for the first time, configure default preferences
-        if !defaults.bool(forKey: Constants.UserDefaults.alreadyLaunched.key) {
+        if !UserDefaultsManager.getAlreadyLaunchedStatus() {
             print("Never launched before")
             
-            defaults.set(true, forKey: Constants.UserDefaults.alreadyLaunched.key)
+            UserDefaultsManager.setAlreadyLaunchedStatus(haveLaunched: true)
             // Setting global notificatino status
-            defaults.set(Constants.UserDefaults.globalNotificationStatus.defaultValue, forKey: Constants.UserDefaults.globalNotificationStatus.key)
+            UserDefaultsManager.setUserGlobalNotificationStatus(isOn: Constants.UserDefaults.globalNotificationStatus.defaultValue)
             // Setting data format
-            defaults.set(Constants.UserDefaults.dataFormat.defaultValue, forKey: Constants.UserDefaults.dataFormat.key)
+            UserDefaultsManager.setDataFormat(dataFormat: Constants.UserDefaults.dataFormat.defaultValue)
             // Setting time format
-            defaults.set(Constants.UserDefaults.timeFormat.defaultValue, forKey: Constants.UserDefaults.timeFormat.key)
+            UserDefaultsManager.setTimeFormat(timeFormat: Constants.UserDefaults.timeFormat.defaultValue)
             // Setting Fiat currency
-            defaults.set( Constants.UserDefaults.fiatCurrency.defaultValue, forKey: Constants.UserDefaults.fiatCurrency.key)
+            UserDefaultsManager.setFiatCurrency(fiatCurrency: Constants.UserDefaults.fiatCurrency.defaultValue)
         }else{
             print("Had launched before")
-            var settings: [String:Any] = [:]
-            
-            settings[Constants.UserDefaults.alreadyLaunched.key] = defaults.bool(forKey: Constants.UserDefaults.alreadyLaunched.key)
-            
-            settings[Constants.UserDefaults.globalNotificationStatus.key] = defaults.bool(forKey: Constants.UserDefaults.globalNotificationStatus.key)
-            
-            settings[Constants.UserDefaults.dataFormat.key] = defaults.string(forKey: Constants.UserDefaults.dataFormat.key)
-            
-            settings[Constants.UserDefaults.timeFormat.key] = defaults.string(forKey: Constants.UserDefaults.timeFormat.key)
-            
-            settings[Constants.UserDefaults.fiatCurrency.key] = defaults.string(forKey: Constants.UserDefaults.fiatCurrency.key)
-            
-            print(settings)
-            
         }
-        
         
         return true
     }
---
+++ CryptoChecker/Managers/UserDefaultsManager.swift
@@ -0,0 +1,74 @@
+//
+//  PreferencesManager.swift
+//  CryptoChecker
+//
+//  Created by Henrikas J on 10/12/2020.
+//
+
+import UIKit
+
+final class UserDefaultsManager {
+    
+    private static let defaults = UserDefaults.standard
+    
+    public static func getAlreadyLaunchedStatus() -> Bool {
+        let returnBool = defaults.bool(forKey: Constants.UserDefaults.alreadyLaunched.key)
+        return returnBool
+    }
+    
+    // Returning global notificatino status from User defaults
+    public static func getUserGlobalNotificationStatus() -> Bool {
+        let returnBool = defaults.bool(forKey: Constants.UserDefaults.globalNotificationStatus.key)
+        return returnBool
+    }
+    
+    // Returning selected date format from User defaults
+    public static func getDateFormat() -> String {
+        let returnString: String = defaults.string(forKey: Constants.UserDefaults.dataFormat.key)
+                                   ?? Constants.UserDefaults.dataFormat.defaultValue
+        
+        return returnString
+    }
+    
+    // Returning selected time format from User defaults
+    public static func getTimeFormat() -> String {
+        let returnString: String = defaults.string(forKey: Constants.UserDefaults.timeFormat.key)
+                                   ?? Constants.UserDefaults.timeFormat.defaultValue
+        
+        return returnString
+    }
+    
+    // Returning selected date format from User defaults
+    public static func getFiatCurrency() -> String {
+        let returnString: String = defaults.string(forKey: Constants.UserDefaults.dataFormat.key)
+                                   ?? Constants.UserDefaults.dataFormat.defaultValue
+        
+        return returnString
+    }
+    
+    // Setting alreadyLaunchedStatus to provided value
+    public static func setAlreadyLaunchedStatus(haveLaunched: Bool){
+        defaults.set(haveLaunched, forKey: Constants.UserDefaults.alreadyLaunched.key)
+    }
+    
+    // Setting globalNotificationStatus to provided value
+    public static func setUserGlobalNotificationStatus(isOn: Bool){
+        defaults.set(isOn, forKey: Constants.UserDefaults.globalNotificationStatus.key)
+    }
+    
+    // Setting dataFormat to provided value
+    public static func setDataFormat(dataFormat: String){
+        defaults.set(dataFormat, forKey: Constants.UserDefaults.dataFormat.key)
+    }
+    
+    // Setting timeFormat to provided value
+    public static func setTimeFormat(timeFormat: String){
+        defaults.set(timeFormat, forKey: Constants.UserDefaults.timeFormat.key)
+    }
+    
+    // Setting fiatCurrency to provided value
+    public static func setFiatCurrency(fiatCurrency: String){
+        defaults.set(fiatCurrency, forKey: Constants.UserDefaults.fiatCurrency.key)
+    }
+    
+}
